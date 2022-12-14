5b175514c08e0c8b141af3a01ac2024ba8c9e140
chping <chping@iowk.com>
2022-12-14 17:32:26 +0800

    doc:添加计划实现功能

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-镜像切换工具
+# 镜像切换工具
+
+计划实现功能
+[] 1：default ：默认create-index
+[] 2:   list 所有可用镜像地址---按照网络延迟排序
+[] 3:   auto 自动选择最优镜像
+[] 4:   use xxx /default/auto/1
+[] 5:   add  手动设置镜像地址
+        -name  xxx  -url=xxxx
+[] 6:   sync 地址备份git仓库地址
+[] 7：
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let  cdir = env::current_dir();
+    println!("Hello, world!{:?}",cdir);
+    let cargo_home = env::var("CARGO_HOME").unwrap();
+    println!("CARGO_HOME:{:?}",cargo_home);
 }
+
+
+
