265c5a1f04df575bdca6345e206fe8bd7603fcc7
chping <chping@iowk.com>
2022-12-15 11:57:02 +0800

    feat:clap指令解析

--- src/args.rs
+++ src/args.rs
@@ -2,33 +2,48 @@ use clap::{Parser, Subcommand};
 
 #[derive(Debug, Clone, Parser)]
 #[command(name = "cargo-zproxy")]
+#[command(bin_name = "cargo")]
 #[command(author = "Cxping. <chping@iowk.com>")]
 #[command(version = "0.1.0")]
 #[command(about = "Switch Change image", long_about = None)]
 #[command(propagate_version = true)]
-pub struct  AppArgs{
-    #[command(subcommand)]
-    command: Option<Commands>,
-}
-
-#[derive(Subcommand,Debug,Clone)]
 pub enum Commands {
-    /// does testing things
-    List{
+    /// list 获取当前可用镜像
+    List {
         #[arg(short, long)]
         list: bool,
+    },
+    /// 自动选择一个镜像地址
+    Auto {
+        #[arg(short, long)]
+        auto: bool,
+    },
+    /// 手动设置一个地址
+    /// set -name xxx  
+    ///     -id  xxx
+    Use {
+        #[arg(short, long)]
+        name: String,
+    },
+    /// 手动添加一个地址
+    /// add -name xxx -url xxxx
+    Add{
+        #[arg(short, long)]
+        name: String,
+        #[arg(short, long)]
+        source:String,
+        #[arg(short, long)]
+        url:String,
+    },
+    //设置同步备份奖项地址
+    /// Sync 
+    /// -url xxx 设置仓库地址
+    /// -repo  同步备份
+    Sync{
+        #[arg(short, long)]
+        sync: String,
+        #[arg(short, long)]
+        repo:bool,
     }
-}
 
-impl  AppArgs{
-    pub fn parse_args(&self){
-        match  &self.command {
-            Some(command) => {
-                println!("这是一个参数{:?}",command);
-            },
-            None =>{
-                println!("未知参数");
-            },
-        }
-    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,53 @@
 use std::{env, io, ffi::OsString};
-use args::AppArgs;
+use std::path::PathBuf;
+use args::Commands;
 use clap::Parser;
-
+use log::Level;
+use log::debug;
 pub mod args;
+
+struct  CargoConfig{
+    cargo_dir:PathBuf,
+}
 
 #[tokio::main]
 async fn main()->io::Result<()> {
     let  args = std::env::args_os();
-    //ArgsOs { inner: ["D:\\chping\\.cargo\\bin\\cargo-zproxy.exe", "zproxy", "-l"] }
-    //解析指令
     let  mut iter:Vec<OsString> = args.into_iter().collect();
     if iter.len() > 1 && iter[1] == "zproxy" {
 		iter.remove(1);
 	}
     //解析指令
-    let app_args= AppArgs::parse_from(iter);
-    app_args.parse_args();
+    let app_args= Commands::parse_from(iter);
+    match app_args {
+        // list  获取前配置项中镜像仓库
+        Commands::List { list } => {
+            println!("-- list ");
+        },
+        // auto  自动切换一个镜像仓库
+        Commands::Auto { auto } => {
+            println!("-- Auto ");
+        },
+        // Use --name  xxx  切换指定名称得镜像仓库
+        Commands::Use { name } => {
+            println!("Add -->Set {}",name);
+        }
+        // --name : 镜像名称（非必须）
+        // --source: 仓库标识--唯一标识
+        // --url : 仓库地址
+        Commands::Add { name,url,source} => {
+            println!("Add -->{},{},{} ",name,url,source);
+        }
+        // sync 同步镜像配置
+        // --url xxx 设置镜像源地址
+        // --repo   下载镜像配置
+        Commands::Sync { sync,repo } => {
+            println!("sync ");
+        }
+
+        _=>{
+            println!("无法指令参数")
+        }
+    };
     Ok(())
 }
